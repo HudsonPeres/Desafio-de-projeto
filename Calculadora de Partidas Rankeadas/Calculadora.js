458f7680a9eb4d8731ec4dcde0c1400b80abfb21
HudsonPeres <hudsonaap@gmail.com>
2023-12-14 09:55:20 +0000

    Desafio DIO Calculadora de Partidas Rankeadas

---
+++ Calculadora de Partidas Rankeadas/Calculadora.js
@@ -0,0 +1,15 @@
+function calcNivel(vitoria, derrota) {
+    const saldoVit = vitoria - derrota;
+    let nivel;
+
+    if (vitorias < 10) nivel = "Ferro";
+    else if (vitorias <= 20) nivel = "Bronze";
+    else if (vitorias <= 50) nivel = "Prata";
+    else if (vitorias <= 80) nivel = "Ouro";
+    else if (vitorias <= 90) nivel = "Diamante";
+    else if (vitorias <= 100) nivel = "Lendário";
+    else nivel = "Imortal";
+
+    console.log ("o Héroi tem um saldo de " + saldoVit + "e está no nível " + calcNivel);
+    
+}
